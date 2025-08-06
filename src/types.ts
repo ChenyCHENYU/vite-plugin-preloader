ef8c8ebd24f668ec41ba59a789c20c370b865ec8
ChenYu <ycyplus@gmail.com>
2025-08-06 23:58:14 +0800

    pref: 精简冗余提示配置

--- src/types.ts
+++ src/types.ts
@@ -6,9 +6,7 @@ export interface PreloadRoute {
 }
 
 export interface PreloaderOptions {
-  routes: (string | PreloadRoute)[]
-  delay?: number
-  showStatus?: boolean
-  statusPosition?: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right'
-  debug?: boolean
+  routes: (string | PreloadRoute)[];
+  delay?: number;
+  debug?: boolean;
 }
